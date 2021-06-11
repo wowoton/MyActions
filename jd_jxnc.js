a18d8bf0ab5c8893f197418e1d3952b3a5e8d9d0
wuzhi03 <zhongmuqiao23970@163.com>
2021-06-11 11:45:55 +0800

    搬运zoo，yangtingxiao更新

--- jd_jxnc.js
+++ jd_jxnc.js
@@ -160,7 +160,7 @@ function requireConfig() {
 
     try {
       let options = {
-        "url": `https://cdn.jsdelivr.net/gh/gitupdate/updateTeam@master/shareCodes/jxnc.txt`,
+        "url": ``,
         "headers": {
           "Accept": "application/json,text/plain, */*",
           "Content-Type": "application/x-www-form-urlencoded",
